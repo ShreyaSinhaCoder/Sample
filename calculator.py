f56a5d1efbe3a204c7411c8c8130c2ccc1f4f702
ShreyaSinhaCoder <85826291+ShreyaSinhaCoder@users.noreply.github.com>
2025-06-14 14:50:51 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+
+
+#Calculator app
+def A(a, b):
+  return a+b 
+
+def B(a,b):
+  return a-b
+
+def C(a, b):
+  return a*b
+
+def D(a,b):
+  if b == 0: 
+    return "Error: Invalid output"
+  else:
+    return a/b 
+
+
+if __name__ == "__main__":
+    value = sys.argv[3]
+    a = sys.argv[1]
+    b = sys.argv[2]
+    diction = { "add" : "A", "subtract" : "B", "mulitply" : "C", "divide" :"D" } 
+    method_run = diction.get(value) 
+    print(method_run(a, b))
